e689d1f965ec5625b0e605edaa7daf2fded95a98
mustaphashykh <mustafaahmedshaikh125@gmail.com>
2023-12-23 18:54:13 +0500

    Fixed a major bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { Alert, Header, LoadingScreen } from "./component/commons"
 import { AboutCarPage, AllListingPage, CompletionPage, KeyInfoPage, Listing, LoginPage, PreparationPage, PricePage, ProductPage, ServiceHistoryPage, SpecificationPage } from "./component/pages"
 import { RootState } from "./features/store"
-import { Route, Routes, useNavigate } from "react-router-dom"
-import axios from "axios"
-import { reviloActions } from "./features/slice"
-import { useEffect } from "react"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const userId = useSelector((state: RootState) => state.userId)
   const showLoader = useSelector((state: RootState) => state.showLoader)
-  const fetchUser = async () => {
-    try {
-      const { data } = await axios.get(`http://localhost:5000/api/v1/users/showMe`, { withCredentials: true });
-      dispatch(reviloActions.setUser(data.user.userId));
-    } catch (error) {
-      dispatch(reviloActions.resetUser())
-      navigate('/')
-    }
-  };
-  useEffect(() => {
-    if (!userId) {
-      fetchUser()
-    }
-  },[])
   return (
     <>
       <div className="w-full relative shadow-xl md:max-w-sm min-h-screen mx-auto text-gray-900">
